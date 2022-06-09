24787be024d93592b835a58431c80eda65472d39
Simone Di Cicco <simonedicicco@Simones-MacBook-Pro-2.local>
2022-06-09 16:25:01 +0200

    chore: adding normalizations

---
+++ normalizers.py
@@ -0,0 +1,25 @@
+import copy
+
+
+def remove_every_nth(lst, n):
+    del lst[n - 1::n]
+    return lst
+
+
+def normalize(data: [], field: str):
+    full_field_set = list(set(range(data[-1][field] + 1)))
+    normalized_cursor = list(map(lambda value: {field: value}, full_field_set))
+    normalized_list = []
+
+    last_useful_data = copy.deepcopy(data[0])
+    for cursor in normalized_cursor:
+        existing_data = next((x for x in data if x[field] == cursor[field]), None)
+        if existing_data is not None:
+            last_useful_data = copy.deepcopy(existing_data)
+            normalized_list.append(existing_data)
+        else:
+            last_useful_data[field] = cursor[field]
+            normalized_list.append(copy.deepcopy(last_useful_data))
+
+    return remove_every_nth(normalized_list, 2)
+
--- services/chartify.py
+++ services/chartify.py
@@ -1,9 +1,11 @@
 from f1_types import F1Repository
 from constants import CHARTS, COLORS
+from normalizers import normalize
 
 
 def fastest_laps(drivers: list[str], data: F1Repository):
     chart = {"chart": CHARTS['Q_FASTLAPS'], "data": []}
+    lengths = []
     for i in range(len(drivers)):
         chart_data = []
         for j in range(len(data.fastest_laps_telemetry[i])):
@@ -11,6 +13,15 @@ def fastest_laps(drivers: list[str], data: F1Repository):
                 "speed": int(data.fastest_laps_telemetry[i].iloc[j]['Speed']),
                 "distance": round(int(data.fastest_laps_telemetry[i].iloc[j]['Distance']))
             })
-        chart['data'].append({"driver": drivers[i], "color": COLORS[i], "data": chart_data})
+        # normalize distance
+        normalized_data = normalize(chart_data, 'distance')
+        chart['data'].append({"driver": drivers[i], "color": COLORS[i], "data": normalized_data})
+        lengths.append(len(normalized_data))
+
+    # align lengths
+    shortest = min(lengths)
+    for i in range(len(chart['data'])):
+        if len(chart['data'][i]['data']) > shortest:
+            chart['data'][i]['data'] = chart['data'][i]['data'][:len(chart['data'][i]['data']) - (len(chart['data'][i]['data']) - shortest)]
 
     return chart
